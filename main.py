20c04423f08c9ff135dd81afca175e444f303990
yuanningma <>
2021-04-20 22:39:53 -0400

    init

---
+++ main.py
@@ -0,0 +1,174 @@
+import os
+import sys
+import argparse
+import re
+from datetime import datetime
+import tensorflow as tf
+
+import hyperparameters as hp
+from models import YourModel, VGGModel
+from preprocess import Datasets
+from skimage.transform import resize
+from tensorboard_utils import \
+        ImageLabelingLogger, ConfusionMatrixLogger, CustomModelSaver
+
+from skimage.io import imread
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+from matplotlib import pyplot as plt
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+def parse_args():
+    """ Perform command-line argument parsing. """
+
+    parser = argparse.ArgumentParser(
+        description="Let's train some neural nets!",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--data',
+        default='.'+os.sep+'data'+os.sep,
+        help='Location where the dataset is stored.')
+    parser.add_argument(
+        '--load-vgg',
+        default='vgg16_imagenet.h5',
+        help='''Path to pre-trained VGG-16 file (only applicable to
+        task 3).''')
+    parser.add_argument(
+        '--load-checkpoint',
+        default=None,
+        help='''Path to model checkpoint file (should end with the
+        extension .h5). Checkpoints are automatically saved when you
+        train your model. If you want to continue training from where
+        you left off, this is how you would load your weights.''')
+    parser.add_argument(
+        '--confusion',
+        action='store_true',
+        help='''Log a confusion matrix at the end of each
+        epoch (viewable in Tensorboard). This is turned off
+        by default as it takes a little bit of time to complete.''')
+    parser.add_argument(
+        '--evaluate',
+        action='store_true',
+        help='''Skips training and evaluates on the test set once.
+        You can use this to test an already trained model by loading
+        its checkpoint.''')
+
+    return parser.parse_args()
+
+def train(model, datasets, checkpoint_path, logs_path, init_epoch):
+    """ Training routine. """
+
+    # Keras callbacks for training
+    callback_list = [
+        tf.keras.callbacks.TensorBoard(
+            log_dir=logs_path,
+            update_freq='batch',
+            profile_batch=0),
+        ImageLabelingLogger(logs_path, datasets),
+        CustomModelSaver(checkpoint_path, ARGS.task, hp.max_num_weights)
+    ]
+
+    # Include confusion logger in callbacks if flag set
+    if ARGS.confusion:
+        callback_list.append(ConfusionMatrixLogger(logs_path, datasets))
+
+    # Begin training
+    model.fit(
+        x=datasets.train_data,
+        validation_data=datasets.test_data,
+        epochs=hp.num_epochs,
+        batch_size=None,
+        callbacks=callback_list,
+        initial_epoch=init_epoch,
+    )
+
+def test(model, test_data):
+    """ Testing routine. """
+
+    # Run model on test set
+    model.evaluate(
+        x=test_data,
+        verbose=1,
+    )
+
+
+def main():
+    """ Main function. """
+
+    time_now = datetime.now()
+    timestamp = time_now.strftime("%m%d%y-%H%M%S")
+    init_epoch = 0
+
+    # If loading from a checkpoint, the loaded checkpoint's directory
+    # will be used for future checkpoints
+    if ARGS.load_checkpoint is not None:
+        ARGS.load_checkpoint = os.path.abspath(ARGS.load_checkpoint)
+
+        # Get timestamp and epoch from filename
+        regex = r"(?:.+)(?:\.e)(\d+)(?:.+)(?:.h5)"
+        init_epoch = int(re.match(regex, ARGS.load_checkpoint).group(1)) + 1
+        timestamp = os.path.basename(os.path.dirname(ARGS.load_checkpoint))
+
+    # If paths provided by program arguments are accurate, then this will
+    # ensure they are used. If not, these directories/files will be
+    # set relative to the directory of run.py
+    if os.path.exists(ARGS.data):
+        ARGS.data = os.path.abspath(ARGS.data)
+    if os.path.exists(ARGS.load_vgg):
+        ARGS.load_vgg = os.path.abspath(ARGS.load_vgg)
+
+    # Run script from location of run.py
+    os.chdir(sys.path[0])
+
+    datasets = Datasets(ARGS.data, ARGS.task)
+
+
+    model = VGGModel()
+    checkpoint_path = "checkpoints" + os.sep + \
+        "vgg_model" + os.sep + timestamp + os.sep
+    logs_path = "logs" + os.sep + "vgg_model" + \
+        os.sep + timestamp + os.sep
+    model(tf.keras.Input(shape=(224, 224, 3)))
+
+    # Print summaries for both parts of the model
+    model.vgg16.summary()
+    # model.head.summary()
+
+    # Load base of VGG model
+    model.vgg16.load_weights(ARGS.load_vgg, by_name=True)
+
+    # Load checkpoints
+    if ARGS.load_checkpoint is not None:
+        if ARGS.task == '1':
+            model.load_weights(ARGS.load_checkpoint, by_name=False)
+        else:
+            model.head.load_weights(ARGS.load_checkpoint, by_name=False)
+
+    # Make checkpoint directory if needed
+    if not ARGS.evaluate and not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    # Compile model graph
+    model.compile(
+        optimizer=model.optimizer,
+        loss=model.loss_fn,
+        metrics=["sparse_categorical_accuracy"])
+
+    if ARGS.evaluate:
+        test(model, datasets.test_data)
+
+        # TODO: change the image path to be the image of your choice by changing
+        # the lime-image flag when calling run.py to investigate
+        # i.e. python run.py --evaluate --lime-image test/Bedroom/image_003.jpg
+        path = ARGS.data + os.sep + ARGS.lime_image
+        LIME_explainer(model, path, datasets.preprocess_fn)
+    else:
+        train(model, datasets, checkpoint_path, logs_path, init_epoch)
+
+
+# Make arguments global
+ARGS = parse_args()
+
+main()
---
+++ model.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+from tensorflow.keras.layers import \
+    Conv2D, MaxPool2D, Dropout, Flatten, Dense
+
+import hyperparameters as hp
+
+class VGGModel(tf.keras.Model):
+    def __init__(self):
+        super(VGGModel, self).__init__()
+
+        # TODO: Select an optimizer for your network (see the documentation
+        #       for tf.keras.optimizers)
+
+        self.optimizer =  tf.keras.optimizers.RMSprop(learning_rate=hp.learning_rate, momentum=hp.momentum)
+
+        # Don't change the below:
+
+        self.vgg16 = [
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
+        ]
+
+        # TODO: Make all layers in self.vgg16 non-trainable. This will freeze the
+        #       pretrained VGG16 weights into place so that only the classificaiton
+        #       head is trained.
+
+        # TODO: Write a classification head for our 15-scene classification task.
+        for layer in self.vgg16:
+            layer.trainable = False
+        # self.head = [
+        #     Flatten(),
+        #     Dense(64, activation="relu"),
+        #     Dropout(0.2),
+        #     Dense(64, activation="relu"),
+        #     Dropout(0.2),
+        #     Dense(15, activation="softmax")
+        # ]
+
+        # Don't change the below:
+        self.vgg16 = tf.keras.Sequential(self.vgg16, name="vgg_base")
+        # self.head = tf.keras.Sequential(self.head, name="vgg_head")
+
+    def call(self, x):
+        """ Passes the image through the network. """
+
+        x = self.vgg16(x)
+        x = self.head(x)
+
+        return x
+
+    @staticmethod
+    def loss_fn(labels, predictions):
+        """ Loss function for model. """
+
+        # TODO: Select a loss function for your network (see the documentation
+        #       for tf.keras.losses)
+
+        func = tf.keras.losses.SparseCategoricalCrossentropy(False)
+        return func(labels, predictions)
